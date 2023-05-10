8c85b084d8418ff9d272907da15c6cbc6ca4e1dc
Daniel Kusi Boateng <daniel.boateng@amalitech.org>
2023-05-10 13:38:31 +0000

    updated

---
+++ src/AdminFolder/BulkLectInfo/Type.ts
@@ -0,0 +1,11 @@
+export type file = [{
+    name: string,
+    details: {
+        firstname: boolean
+    lastname: boolean
+    email: boolean
+    number: boolean
+    qualification: boolean
+    department: boolean
+    }
+}]
